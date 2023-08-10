6455542e9ad5b35d65f9be2c2dbd436f28f91801
Toni Schmitt <tonischmitt@online.de>
2023-08-10 17:57:47 +0200

    fix: use Path.Join for portable unit tests
    Signed-off-by: Toni Schmitt <tonischmitt@online.de>

--- src/PackerX.Backend/PackerX.Backend.UnitTests/TestData.cs
+++ src/PackerX.Backend/PackerX.Backend.UnitTests/TestData.cs
@@ -8,10 +8,15 @@ public static class TestData
     private static readonly string? s_baseDirectory = GetBaseDirectory();
 
     private static readonly string s_testFilesDirectory =
-        @$"{s_baseDirectory}\TestFiles";
+        Path.Join(
+            s_baseDirectory,
+            "TestFiles"
+        );
 
-    public static readonly string OriginalFileName =
-        @$"{s_testFilesDirectory}\org-file";
+    public static readonly string OriginalFileName = Path.Join(
+        s_testFilesDirectory,
+        "org-file"
+    );
 
     private static string GetBaseDirectory()
     {
@@ -36,31 +41,49 @@ public static class TestData
     public static class EncodedTestData
     {
         private static readonly string s_base =
-            $@"{s_testFilesDirectory}\Encoder";
+            Path.Join(
+                s_testFilesDirectory,
+                "Encoder"
+            );
 
         public static readonly string ActualEncodedFileName =
-            $@"{s_base}\actual-enc-file{Constants.EncodingFileExtension}";
+            Path.Join(
+                s_base,
+                $"actual-enc-file{Constants.EncodingFileExtension}"
+            );
 
         public static readonly string ExpectedEncodedFileName =
-            $@"{s_base}\expected-enc-file{Constants.EncodingFileExtension}";
+            Path.Join(
+                s_base,
+                "expected-enc-file.ttpack"
+            );
     }
 
     public static class DecodedTestData
     {
         private static readonly string
-            s_base = $@"{s_testFilesDirectory}\Decoder";
+            s_base = Path.Join(
+                s_testFilesDirectory,
+                "Decoder"
+            );
 
         public static readonly string EncodedFileName =
-            $@"{s_base}\enc-file.ttpack";
+            Path.Join(
+                s_base,
+                "enc-file.ttpack"
+            );
 
         public static readonly string ActualDecodedFileName =
-            $@"{s_base}\actual-decoded-file";
+            Path.Join(
+                s_base,
+                "actual-decoded-file"
+            );
     }
 
     public static class ErrorTestData
     {
 
-        public const string InvalidFileName = @"SL:KDJFSDF:LKSDJFSDF";
+        public const string InvalidFileName = "SL:KDJFSDF:LKSDJFSDF";
 
         public static readonly string NonExistentFileName =
             @"X:\this\path\does\not\exists";
